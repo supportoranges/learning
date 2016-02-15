e01aff76f7d6100753593fef1919f668be4dd08f
techsupport <techsupport@new-host.home>
2016-02-15 12:59:19 -0500

    first commit

---
+++ Sites/scriptController.js
@@ -0,0 +1,31 @@
+var myAppModule = angular.module("myApp", []);
+
+myAppModule.controller('mainController', function() //controller for the view; constructor function
+{
+
+})
+
+//ng_controller was used to attach the controller to the DOM , instantiates it too using the Ctor
+//a new child scope will be created and made available as an injectable parameter to the ctor function!
+//controller sets up initial state of the $scope
+//add behavior to the $scope
+//only business logic should be in the controller, NO PRESENTATION LOGiC
+
+//databinding and directives are used to encapsulate DOM manipulation
+//not for formatting input USe angular form controls instead
+//filter output - use angular FILTERS instead
+//share code or state across controllers - USE ANGULAR services instead
+//manage the life=cycle of other components (to not use to create service instances)
+myAppModule.filter('greet', function()  {  //just say 'ITs a FUNCTION!'
+	
+	return function(name) {
+
+		return 'Hello, ' + name + '!';
+	}
+})
+
+var searchPeople = function(firstname, lastName, height, age, occupation ) 
+{
+	return 'Jane Doe';
+}
+console.log(searchPeople(1,2,3,4,5));
